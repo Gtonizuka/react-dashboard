1c384c86c955a1881debb99c1c6f9e26ef558263
Mirko Garozzo <mirkogarozzo@hotmail.it>
2020-02-18 19:36:06 -0500

    Added moment

--- src/components/DatePickerCustomComponent/DatePickerCustomComponent.js
+++ src/components/DatePickerCustomComponent/DatePickerCustomComponent.js
@@ -2,21 +2,23 @@ import React, { useState } from "react";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import './DatePickerCustomComponent.scss'
+import "./DatePickerCustomComponent.scss";
 
-
-const DatePickerCustom = ({getCalendarDate}) => {
-    const [startDate, setStartDate] = useState(new Date('1-1-2020'));
+const DatePickerCustom = ({ getCalendarDate }) => {
+  const [startDate, setStartDate] = useState(new Date("2020-01-01T08:00:00Z"));
   return (
     <DatePicker
       selected={startDate}
-      onChange={date => {getCalendarDate(date); setStartDate(date)}}
+      onChange={date => {
+        getCalendarDate(date);
+        setStartDate(date);
+      }}
       onClick={getCalendarDate}
-      minDate={new Date("01-01-2020")}
-      maxDate={new Date("01-31-2020")}
+      minDate={new Date("2020-01-01T08:00:00Z")}
+      maxDate={new Date("2020-01-31T08:00:00Z")}
       placeholderText="Select a date in January"
     />
   );
-  };
+};
 
 export default DatePickerCustom;
